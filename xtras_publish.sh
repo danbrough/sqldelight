c2f8a1df1f384d13cc85fa7aa7975c79834c094c
Dan Brough <dan@danbrough.org>
2022-10-01 09:48:36 +1300

    add sonatype configuration

--- xtras_publish.sh
+++ xtras_publish.sh
@@ -2,5 +2,12 @@
 
 cd "$(dirname "$0")" 
 
-echo pwd `pwd`
+[ -z "$SONATYPE_USER" ] && echo SONATYPE_USER not set && exit 1
+[ -z "$SONATYPE_PASSWORD" ] && echo SONATYPE_PASSWORD not set && exit 1
+[ -z "$SONATYPE_REPO_ID" ] && echo SONATYPE_REPO_ID not set && exit 1
 
+if [ "$(uname)" = "Darwin" ]; then 
+	
+else
+	./gradlew  publishAllPublicationsToSonatypeRepository
+fi
