0020c96a88037e9d46e300ececa22b390a24f9cc
Dan Brough <dan@danbrough.org>
2022-10-01 10:02:19 +1300

    update

--- xtras_publish.sh
+++ xtras_publish.sh
@@ -6,8 +6,9 @@ cd "$(dirname "$0")"
 [ -z "$SONATYPE_PASSWORD" ] && echo SONATYPE_PASSWORD not set && exit 1
 [ -z "$SONATYPE_REPO_ID" ] && echo SONATYPE_REPO_ID not set && exit 1
 
-if [ "$(uname)" = "Darwin" ]; then 
-	
-else
-	./gradlew  publishAllPublicationsToSonatypeRepository
+if [ "$(uname)" != "Darwin" ]; then
+		./gradlew  publishAllPublicationsToSonatypeRepository
+		exit $?
 fi
+
+
